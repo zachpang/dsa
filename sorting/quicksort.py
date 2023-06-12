5457be93971a96b4bc7e6b026abdcc0647ecce4a
zachpang <zachery.pang@gmail.com>
2023-06-12 19:35:02 +0800

    Add quicksort using hoare partitioning

--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -27,3 +27,48 @@ def quicksort_lomuto(arr, lo, hi):
 
     quicksort_lomuto(arr, lo, partitioning_index - 1)
     quicksort_lomuto(arr, partitioning_index + 1, hi)
+
+
+def median_of_3(a, b, c):
+    if a <= b <= c or c <= b <= a:
+        return b
+    elif b <= a <= c or c <= a <= b:
+        return a
+    else:
+        return c
+
+
+def hoare_partition(arr, lo, hi):
+    pivot = median_of_3(arr[lo], arr[(lo + hi) // 2], arr[hi])
+
+    l, r = lo - 1, hi + 1
+
+    while True:
+        l += 1
+        while arr[l] < pivot:
+            l += 1
+
+        r -= 1
+        while arr[r] > pivot:
+            r -= 1
+
+        if l >= r:
+            # l or r can be returned as the partitioning index.
+            # we need to be aware of which partition left or right the
+            # index belongs to. It will be passed into the next
+            # recursion calls as lo/hi args
+
+            # when l and r cross, r is in the left partition
+            return r
+
+        arr[l], arr[r] = arr[r], arr[l]
+
+
+def quicksort_hoare(arr, lo, hi):
+    if lo >= hi:
+        return
+
+    partitioning_index = hoare_partition(arr, lo, hi)
+
+    quicksort_hoare(arr, lo, partitioning_index)
+    quicksort_hoare(arr, partitioning_index + 1, hi)
--- sorting/test_quicksort.py
+++ sorting/test_quicksort.py
@@ -1,4 +1,4 @@
-from quicksort import quicksort_lomuto
+from quicksort import quicksort_lomuto, quicksort_hoare
 
 
 def test_quicksort_lomuto():
@@ -44,3 +44,48 @@ def test_quicksort_lomuto():
 
 
 test_quicksort_lomuto()
+
+
+def test_quicksort_hoare():
+    arr = []
+    quicksort_hoare(arr, 0, len(arr) - 1)
+    assert arr == []
+
+    arr = [5]
+    quicksort_hoare(arr, 0, len(arr) - 1)
+    assert arr == [5]
+
+    arr = [3, 7, 2, 5, 2, 9, 7]
+    quicksort_hoare(arr, 0, len(arr) - 1)
+    assert arr == [2, 2, 3, 5, 7, 7, 9]
+
+    arr = [1, 2, 3, 4, 5]
+    quicksort_hoare(arr, 0, len(arr) - 1)
+    assert arr == [1, 2, 3, 4, 5]
+
+    arr = [5, 4, 3, 2, 1]
+    quicksort_hoare(arr, 0, len(arr) - 1)
+    assert arr == [1, 2, 3, 4, 5]
+
+    arr = [-2, 4, -7, 0, -1]
+    quicksort_hoare(arr, 0, len(arr) - 1)
+    assert arr == [-7, -2, -1, 0, 4]
+
+    arr = [1000000, 999999, 100000, 99999]
+    quicksort_hoare(arr, 0, len(arr) - 1)
+    assert arr == [99999, 100000, 999999, 1000000]
+
+    arr = [9, 1, 5, 3, 7, 2]
+    quicksort_hoare(arr, 0, len(arr) - 1)
+    assert arr == [1, 2, 3, 5, 7, 9]
+
+    arr = [8, 8, 8, 8, 8]
+    quicksort_hoare(arr, 0, len(arr) - 1)
+    assert arr == [8, 8, 8, 8, 8]
+
+    arr = [2, 1, 2, 1, 2, 1, 1]
+    quicksort_hoare(arr, 0, len(arr) - 1)
+    assert arr == [1, 1, 1, 1, 2, 2, 2]
+
+
+test_quicksort_hoare()
